3011c552b3b6af20025f15ab61f2832aae51d405
gabriel correa <gabriel2pk@hotmail.com>
2026-08-25 22:33:54 -0400

    refactor

--- rootcauze1.cpp
+++ rootcauze1.cpp
@@ -10,6 +10,23 @@
 #include <thread>
 
 #pragma comment(lib, "pdh.lib")
+struct ProcessInfo
+{
+	std::string name;
+	DWORD pid;
+	DWORD parentPid;
+	SIZE_T ramMB;
+
+};
+struct GlobalMetrics
+{
+	double cpuUsage;
+	double pageFaults;
+	double diskLatency;
+	double diskQueue;
+	double pagingUsage;
+
+};
 
 PDH_HCOUNTER AddPDHCounter(PDH_HQUERY query, const char* counterPath)
 {
@@ -23,14 +40,7 @@ PDH_FMT_COUNTERVALUE GetPDHCounterValue(PDH_HCOUNTER counter)
 	PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, NULL, &counterValue);
 	return counterValue;
 }
-struct ProcessInfo
-{
-	std::string name;
-	DWORD pid;
-	DWORD parentPid;
-	SIZE_T ramMB;
 
-};
 bool comparator(const ProcessInfo& a, const ProcessInfo& b) {
 	return a.ramMB > b.ramMB;
 }
@@ -40,12 +50,9 @@ void threadCollector(PDH_HCOUNTER counter){
 	std::cout << "Value: " << value << std::endl;
 }
 
-int main()
-{
-	HANDLE sniffer = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	PROCESSENTRY32 processList;
-	processList.dwSize = sizeof(PROCESSENTRY32);
-	BOOL bProcess = Process32First(sniffer, &processList);
+ GlobalMetrics CollectMetrics() {
+
+	GlobalMetrics metrics;
 	PDH_HQUERY query;
 	PdhOpenQueryA(NULL, NULL, &query);
 	PDH_HCOUNTER counter = AddPDHCounter(query, "\\Processor(_total)\\% Processor Time");
@@ -56,49 +63,62 @@ int main()
 	PdhCollectQueryData(query);
 	Sleep(1000);
 	PdhCollectQueryData(query);
-	double counterValue = GetPDHCounterValue(counter).doubleValue;
-	double memCounterValue = GetPDHCounterValue(memCounter).doubleValue;
-	double diskLatencyValue = GetPDHCounterValue(diskLatency).doubleValue;
-	double diskQueueValue = GetPDHCounterValue(diskQueue).doubleValue;
-	double pagingUsageValue = GetPDHCounterValue(pagingUsage).doubleValue;	
-	std::cout << "CPU: " << counterValue << "% | PageFaults: " << memCounterValue
-		<< " | DiskLatency: " << diskLatencyValue << "ms | DiskQueue: " << diskQueueValue
-		<< " | PagingUsage: " << pagingUsageValue << "%" << std::endl;
-	
-	std::vector<ProcessInfo> processes;
+	metrics.cpuUsage = GetPDHCounterValue(counter).doubleValue;
+	metrics.pageFaults = GetPDHCounterValue(memCounter).doubleValue;
+	metrics.diskLatency = GetPDHCounterValue(diskLatency).doubleValue;
+	metrics.diskQueue = GetPDHCounterValue(diskQueue).doubleValue;
+	metrics.pagingUsage = GetPDHCounterValue(pagingUsage).doubleValue;
+	PdhCloseQuery(query);
+	return metrics;
+}
+ std::vector<ProcessInfo> collectProcesses() {
+	 HANDLE sniffer = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	 PROCESSENTRY32 processList;
+	 processList.dwSize = sizeof(PROCESSENTRY32);
+	 BOOL bProcess = Process32First(sniffer, &processList);
+	 std::vector<ProcessInfo> processes;
 
-	
-	
-	while (bProcess)
-	{
-		
-		HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, processList.th32ProcessID);
-		if (processHandle == NULL)
-		{
-			//std::cout << "Error: " << GetLastError() << std::endl;
-			bProcess = Process32Next(sniffer, &processList);
-			continue;
-		}
-		PROCESS_MEMORY_COUNTERS pmc;
-		GetProcessMemoryInfo(processHandle, &pmc, sizeof(pmc));
-		CloseHandle(processHandle);
-		ProcessInfo p;
-		p.name = processList.szExeFile;
-		p.pid = processList.th32ProcessID;
-		p.parentPid = processList.th32ParentProcessID;
-		p.ramMB = pmc.WorkingSetSize / (1024 * 1024);
-		processes.push_back(p);
-		bProcess = Process32Next(sniffer, &processList);
-		
-	}
+	 while (bProcess) {
+		 HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, processList.th32ProcessID);
+		 if (processHandle == NULL)
+		 {
+			 //std::cout << "Error: " << GetLastError() << std::endl;
+			 bProcess = Process32Next(sniffer, &processList);
+			 continue;
+
+		 }
+		 PROCESS_MEMORY_COUNTERS pmc;
+		 GetProcessMemoryInfo(processHandle, &pmc, sizeof(pmc));
+		 CloseHandle(processHandle);
+		 ProcessInfo p;
+		 p.name = processList.szExeFile;
+		 p.pid = processList.th32ProcessID;
+		 p.parentPid = processList.th32ParentProcessID;
+		 p.ramMB = pmc.WorkingSetSize / (1024 * 1024);
+		 processes.push_back(p);
+		 bProcess = Process32Next(sniffer, &processList);
+		 
+	 }
+	 CloseHandle(sniffer);
+
+
+	 return processes;
+ }
+
+ int main()
+ {
+	GlobalMetrics metrics = CollectMetrics(); 
+	std::vector<ProcessInfo> processes = collectProcesses();
+	std::cout << "CPU: " << metrics.cpuUsage << "% | PageFaults: " << metrics.pageFaults
+		<< " | DiskLatency: " << metrics.diskLatency << "ms | DiskQueue: " << metrics.diskQueue
+		<< " | PagingUsage: " << metrics.pagingUsage << "%" << std::endl;
 	std::sort(processes.begin(), processes.end(), comparator);
 	for (size_t i = 0; i < 20&& i <processes.size();i++)
 	{
 		std::cout << processes[i].name << " (PID: " << processes[i].pid << ") | Parent: " << processes[i].parentPid << " | RAM: " << processes[i].ramMB << "MB" << std::endl;
 
 	}
-	PdhCloseQuery(query);
-	CloseHandle(sniffer);
+	
 	return 0;
 
 }
