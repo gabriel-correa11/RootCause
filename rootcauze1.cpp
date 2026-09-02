7e8f4342c42a63c81397923b65374fc3672d5f3e
gabriel correa <gabriel2pk@hotmail.com>
2026-09-02 19:23:15 -0400

    Feat: Begin Cpu per process collection with pdh wildcard expasion

--- rootcauze1.cpp
+++ rootcauze1.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#define WIN32_LEAN_AND_MEAN 1
+#include <iostream>
 #include <Windows.h>
 #include <stdio.h>
 #include <pdh.h>
@@ -8,6 +9,7 @@
 #include <vector>
 #include <algorithm>
 #include <thread>
+#include <mutex>
 
 #pragma comment(lib, "pdh.lib")
 struct ProcessInfo
@@ -16,7 +18,9 @@ struct ProcessInfo
 	DWORD pid;
 	DWORD parentPid;
 	SIZE_T ramMB;
-
+	double cpuUsagePercent;
+	double hardPageFaultsPerSec;
+	SIZE_T ioBytesTotal;
 };
 struct GlobalMetrics
 {
@@ -44,13 +48,13 @@ PDH_FMT_COUNTERVALUE GetPDHCounterValue(PDH_HCOUNTER counter)
 bool comparator(const ProcessInfo& a, const ProcessInfo& b) {
 	return a.ramMB > b.ramMB;
 }
-void threadCollector(PDH_HCOUNTER counter){
+void threadCollector(PDH_HCOUNTER counter) {
 	std::cout << "thread started" << std::endl;
 	double value = GetPDHCounterValue(counter).doubleValue;
 	std::cout << "Value: " << value << std::endl;
 }
 
- GlobalMetrics CollectMetrics() {
+GlobalMetrics CollectMetrics() {
 
 	GlobalMetrics metrics;
 	PDH_HQUERY query;
@@ -71,54 +75,85 @@ void threadCollector(PDH_HCOUNTER counter){
 	PdhCloseQuery(query);
 	return metrics;
 }
- std::vector<ProcessInfo> collectProcesses() {
-	 HANDLE sniffer = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	 PROCESSENTRY32 processList;
-	 processList.dwSize = sizeof(PROCESSENTRY32);
-	 BOOL bProcess = Process32First(sniffer, &processList);
-	 std::vector<ProcessInfo> processes;
+std::vector<ProcessInfo> collectProcesses() {
+	HANDLE sniffer = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 processList;
+	processList.dwSize = sizeof(PROCESSENTRY32);
+	BOOL bProcess = Process32First(sniffer, &processList);
+	std::vector<ProcessInfo> processes;
+	DWORD bufferSize = 0;
+	PdhExpandCounterPathW(L"\\Process(*)\\% Processor Time", NULL, &bufferSize );
+	PWSTR paths = (PWSTR)malloc(bufferSize * sizeof(WCHAR));
+	PdhExpandCounterPathW(L"\\Process(*)\\% Processor Time", paths, &bufferSize );
 
-	 while (bProcess) {
-		 HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, processList.th32ProcessID);
-		 if (processHandle == NULL)
-		 {
-			 //std::cout << "Error: " << GetLastError() << std::endl;
-			 bProcess = Process32Next(sniffer, &processList);
-			 continue;
+	while (bProcess) {
+		HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, processList.th32ProcessID);
+		if (processHandle == NULL)
+		{
+			//std::cout << "Error: " << GetLastError() << std::endl;
+			bProcess = Process32Next(sniffer, &processList);
+			continue;
 
-		 }
-		 PROCESS_MEMORY_COUNTERS pmc;
-		 GetProcessMemoryInfo(processHandle, &pmc, sizeof(pmc));
-		 CloseHandle(processHandle);
-		 ProcessInfo p;
-		 p.name = processList.szExeFile;
-		 p.pid = processList.th32ProcessID;
-		 p.parentPid = processList.th32ParentProcessID;
-		 p.ramMB = pmc.WorkingSetSize / (1024 * 1024);
-		 processes.push_back(p);
-		 bProcess = Process32Next(sniffer, &processList);
-		 
-	 }
-	 CloseHandle(sniffer);
+		}
+		PROCESS_MEMORY_COUNTERS pmc;
+		GetProcessMemoryInfo(processHandle, &pmc, sizeof(pmc));
+		CloseHandle(processHandle);
+		ProcessInfo p;
+		p.name = processList.szExeFile;
+		p.pid = processList.th32ProcessID;
+		p.parentPid = processList.th32ParentProcessID;
+		p.ramMB = pmc.WorkingSetSize / (1024 * 1024);
+		p.cpuUsagePercent = 0.0; 
+		p.hardPageFaultsPerSec = 0.0;
+		p.ioBytesTotal = 0;
+		processes.push_back(p);
+		bProcess = Process32Next(sniffer, &processList);
+
+	}
+	CloseHandle(sniffer);
 
 
-	 return processes;
- }
+	return processes;
+}
+std::mutex mtx;
 
- int main()
- {
-	GlobalMetrics metrics = CollectMetrics(); 
+void collectMetricsThread() {
+
+	GlobalMetrics metrics = CollectMetrics();
+	std::lock_guard<std::mutex> lock(mtx);
+	std::cout << "CPU Usage: " << metrics.cpuUsage << "%" << std::endl;
+
+
+}
+void collectProcessesThread() {
+	std::vector<ProcessInfo> processes = collectProcesses();
+	std::lock_guard<std::mutex> lock(mtx);
+	for (size_t i = 0; i < 20 && i < processes.size(); i++)
+	{
+		std::cout << "Process Name: " << processes[i].name << " | PID: " << processes[i].pid << " | Parent PID: " << processes[i].parentPid << " | RAM Usage: "
+			<< processes[i].ramMB << "MB" << std::endl;
+	}
+}
+
+
+int main()
+{
+	std::thread metricsThread(collectMetricsThread);
+	std::thread processesThread(collectProcessesThread);
+	metricsThread.join();
+	processesThread.join();
+
+	GlobalMetrics metrics = CollectMetrics();
 	std::vector<ProcessInfo> processes = collectProcesses();
 	std::cout << "CPU: " << metrics.cpuUsage << "% | PageFaults: " << metrics.pageFaults
 		<< " | DiskLatency: " << metrics.diskLatency << "ms | DiskQueue: " << metrics.diskQueue
 		<< " | PagingUsage: " << metrics.pagingUsage << "%" << std::endl;
 	std::sort(processes.begin(), processes.end(), comparator);
-	for (size_t i = 0; i < 20&& i <processes.size();i++)
+	for (size_t i = 0; i < 20 && i < processes.size();i++)
 	{
 		std::cout << processes[i].name << " (PID: " << processes[i].pid << ") | Parent: " << processes[i].parentPid << " | RAM: " << processes[i].ramMB << "MB" << std::endl;
 
 	}
-	
-	return 0;
 
+	return 0;
 }
